964c40031deb2f498e30eaf1a09a3a7b4ae860e1
Gucci HO <tho22@student.aau.dk>
2024-03-19 11:11:15 +0100

    Fixed square image PC representation (b/w->greyscale))

--- Pøbelvælden.py
+++ Pøbelvælden.py
@@ -95,13 +95,21 @@ def inverse_polar_transform(magnitude, phase):
 
 # Function to convert audio to image
 def audio_to_image(adjusted_magnitude, adjusted_phase, magnitude_scale=1.0):
-
     image_size = 256
+
+    # Normalize magnitude to [0, 255]
     normalized_magnitude = ((adjusted_magnitude - np.min(adjusted_magnitude)) /
                             (np.max(adjusted_magnitude) - np.min(adjusted_magnitude)) * 255).astype(np.uint8)
 
+    # Normalize phase to [-π, π]
+    normalized_phase = adjusted_phase - np.pi
+
+    # Scale phase to [0, 255]
+    normalized_phase = ((normalized_phase - np.min(normalized_phase)) /
+                        (np.max(normalized_phase) - np.min(normalized_phase)) * 255).astype(np.uint8)
+
     resized_magnitude = np.resize(normalized_magnitude, (image_size // 2, image_size))
-    resized_phase = np.resize(adjusted_phase, (image_size // 2, image_size))
+    resized_phase = np.resize(normalized_phase, (image_size // 2, image_size))
 
     polar_image = resized_phase
     magnitude_image = resized_magnitude * magnitude_scale
@@ -109,7 +117,6 @@ def audio_to_image(adjusted_magnitude, adjusted_phase, magnitude_scale=1.0):
     combined_image = np.vstack((magnitude_image, polar_image))
     combined_image = Image.fromarray(combined_image.astype(np.uint8)).resize((image_size, image_size))
     combined_image.save("Output_Image.png")
-
 
     return combined_image
 
@@ -146,7 +153,7 @@ def plot_reconstructed_audio(reconstructed_audio, sr):
 # Function to perform the main operations
 def main():
     magnitude_scale = 1.0
-    phase_shift = np.pi/2
+    phase_shift = 0
 
     audio_file = 'GI_GMF_B3_353_20140520_n.wav'
     y, sr = librosa.load(audio_file, sr=None)
