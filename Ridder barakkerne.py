36de395f6dc27a1b8a5e9e9596c2dda65b1f2407
Goodest person <mflens22@student.aau.dk>
2024-04-23 11:01:10 +0200

    Phase changed

--- Ridder barakkerne.py
+++ Ridder barakkerne.py
@@ -136,7 +136,7 @@ def audio_to_image(magnitude, phase, contrast_scale, MaxMag):
 
 def image_to_audio(sr, contrast_scale, MaxMag):
 
-    image = Image.open('1st pixel change gray.png')
+    image = Image.open('PhaseChange White.png')
     image_array = np.array(image)
     image_array = np.double(image_array)
 
