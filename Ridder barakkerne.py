fbc47a66290ff3960669790ad280f3be72db3b4c
Goodest person <mflens22@student.aau.dk>
2024-05-02 14:14:15 +0200

    Bass and low-midrange pictures

--- Ridder barakkerne.py
+++ Ridder barakkerne.py
@@ -136,7 +136,7 @@ def audio_to_image(magnitude, phase, contrast_scale, MaxMag):
 
 def image_to_audio(sr, contrast_scale, MaxMag):
 
-    image = Image.open('Magnitude Changes/40p l2 39-50.png')
+    image = Image.open('Magnitude Changes/1 pixel change/41p l2 99c.png')
     image_array = np.array(image)
     image_array = np.double(image_array)
 
