f8498c42c8a16ced1f03fe53f0d1a2ea4d82e0a9
Markus <mflens22@student.aau.dk>
2024-08-09 15:23:23 +0200

    Interface color fun

--- Ridder barakkerne.py
+++ Ridder barakkerne.py
@@ -12,33 +12,45 @@ root = Tk()
 root.title("App prototype")
 root.geometry("500x500")
 
+def select():
+    root.filename = filedialog.askopenfilename(
+        initialdir="C:/Users/marku/OneDrive - Aalborg Universitet/Githubs/P4-Audio", title="select a file",
+        filetypes=(("PNG files", "*.png"),))
+    myLabel = Label(imgFrame, text=root.filename).pack()
+    myImage = ImageTk.PhotoImage(PIL.Image.open(root.filename))
+    imageLabel = Label(imgFrame, image=myImage).pack()
+    submit()
+    clear()
+
+
 def clear():
-    My_text.delete(1.0,END)
+    DescriptText.delete(1.0,END)
 def submit():
-    My_label.config(text=My_text.get(1.0, END))
+    imgFrame.config(text=DescriptText.get(1.0, END))
+
+
+#A Grid frame for buttons
+buttonframe= LabelFrame(root, text="Click these buttons i fucking dare you >:(", padx=5, pady=5, background="tomato")
+buttonframe.pack()
+
+#Select button for choosing a picture
+Selectbtn=Button(buttonframe, text="Select picture", command=select, background="tomato")
+Selectbtn.grid(row=0, column=2)
+
+#Clear button for clearing any text
+clear_button=Button(buttonframe, text="Clear text box", command=clear)
+clear_button.grid(row=0, column=0)
+
+#Submit button for setting the text into the label below grid frame for buttons
+Submit=Button(buttonframe, text="Submit text", command=submit)
+Submit.grid(row=0, column=1)
+
+DescriptText = Text(root, width=50, height=2, background="chocolate3")
+DescriptText.pack(pady=5)
 
 imgFrame= LabelFrame(root, text="This is image frame", padx=5, pady=5)
 imgFrame.pack(padx=10, pady=10)
 
-My_text = Text(root, width=50, height=2)
-My_text.pack(pady=5)
-
-buttonframe= Frame(root)
-buttonframe.pack()
-
-clear_button=Button(buttonframe, text="Clear screen", command=clear)
-clear_button.grid(row=0, column=0)
-
-Submit=Button(buttonframe, text="Submit text", command=submit)
-Submit.grid(row=0, column=1)
-
-My_label= Label(root, text='')
-My_label.pack(pady=5)
-
-root.filename = filedialog.askopenfilename(initialdir="C:/Users/marku/OneDrive - Aalborg Universitet/Githubs/P4-Audio", title="select a file", filetypes=(("PNG files", "*.png"),))
-myLabel = Label(imgFrame, text=root.filename).pack()
-myImage = ImageTk.PhotoImage(PIL.Image.open(root.filename))
-imageLabel= Label(imgFrame, image=myImage).pack()
 
 
 mainloop()
