9f7e1957113383d015feac576d6c7e2f64c5f9b1
Goodest person <mflens22@student.aau.dk>
2024-08-12 14:32:45 +0200

    Interface load image

--- Ridder barakkerne.py
+++ Ridder barakkerne.py
@@ -7,49 +7,56 @@ from PIL import Image, ImageTk
 import PIL.Image
 from tkinter import *
 from tkinter import filedialog, Label, Tk
+import cv2
 
 root = Tk()
 root.title("App prototype")
 root.geometry("500x500")
 
+LoadImage = None
+
 def select():
+    global LoadImage
+    global DisplayImage
     root.filename = filedialog.askopenfilename(
         initialdir="C:/Users/marku/OneDrive - Aalborg Universitet/Githubs/P4-Audio", title="select a file",
-        filetypes=(("PNG files", "*.png"),))
-    myLabel = Label(imgFrame, text=root.filename).pack()
-    myImage = ImageTk.PhotoImage(PIL.Image.open(root.filename))
-    imageLabel = Label(imgFrame, image=myImage).pack()
-    submit()
-    clear()
+        filetypes=(("PNG files", "*.png"), ("All files", "*"))
+    )
+    CV2Image = cv2.imread(root.filename, cv2.IMREAD_UNCHANGED)
+    if CV2Image.dtype == np.uint16:
+        CV2Image = (CV2Image / 256).astype('uint8')
+    CV2Image = cv2.cvtColor(CV2Image, cv2.COLOR_BGR2RGB)
+    CV2Image = PIL.Image.fromarray(CV2Image)
+    LoadImage = ImageTk.PhotoImage(CV2Image)
+    DisplayImage = Label(ImageFrame, image=LoadImage)
+    DisplayImage.pack()
 
+def Apply():
+    #global LoadImage
+    #LoadImage = None
+    DisplayImage.destroy()
 
-def clear():
-    DescriptText.delete(1.0,END)
-def submit():
-    imgFrame.config(text=DescriptText.get(1.0, END))
+OverFrame = Frame(root, pady=5, padx=5)
+OverFrame.pack()
 
+SliderFrame = LabelFrame(OverFrame,text="Adjust these sliders", pady=5, padx=5)
+SliderFrame.grid(row=0, column=0)
 
-#A Grid frame for buttons
-buttonframe= LabelFrame(root, text="Click these buttons i fucking dare you >:(", padx=5, pady=5, background="tomato")
-buttonframe.pack()
+Slider1=Scale(SliderFrame, from_=0, to=200, orient=HORIZONTAL, troughcolor="darkgreen", background="green")
+Slider1.grid(row=0, column=0)
+Slider1=Scale(SliderFrame, from_=0, to=100, orient=HORIZONTAL, troughcolor="darkblue", background="blue")
+Slider1.grid(row=1, column=0)
+Slider1=Scale(SliderFrame, from_=0, to=50, tickinterval=15, orient=HORIZONTAL, troughcolor="darkred", background="red")
+Slider1.grid(row=2, column=0)
 
-#Select button for choosing a picture
-Selectbtn=Button(buttonframe, text="Select picture", command=select, background="tomato")
-Selectbtn.grid(row=0, column=2)
+Btn = Button(SliderFrame, text="Apply filter", command=Apply)
+Btn.grid(row=0, column=1)
 
-#Clear button for clearing any text
-clear_button=Button(buttonframe, text="Clear text box", command=clear)
-clear_button.grid(row=0, column=0)
+ImageFrame = LabelFrame(OverFrame, text="This is the image", padx=5, pady=5)
+ImageFrame.grid(row=0, column=1)
 
-#Submit button for setting the text into the label below grid frame for buttons
-Submit=Button(buttonframe, text="Submit text", command=submit)
-Submit.grid(row=0, column=1)
-
-DescriptText = Text(root, width=50, height=2, background="chocolate3")
-DescriptText.pack(pady=5)
-
-imgFrame= LabelFrame(root, text="This is image frame", padx=5, pady=5)
-imgFrame.pack(padx=10, pady=10)
+Btn2 = Button(ImageFrame, text="Test", command=select)
+Btn2.pack()
 
 
 
