af6013c883dfc189d237090c6f8fa2f5a4e5fbae
Markus <mflens22@student.aau.dk>
2024-08-15 14:03:30 +0200

    Interface ready for filter implementation (maybe)

--- Ridder barakkerne.py
+++ Ridder barakkerne.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
@@ -15,17 +16,24 @@ root.title("App prototype")
 root.geometry("500x500")
 
 LoadImage = None
-DisplayImage = None
+filterImage = None
+transformed_image = None
 
 def select():
     mb.showinfo("Choose", "Choose a picture you want to apply filters to")
 
     global LoadImage
     global DisplayImage
+    global filterImage
+    global CV2Image
+    global transformed_image
     root.filename = filedialog.askopenfilename(
         initialdir="C:/Users/marku/OneDrive - Aalborg Universitet/Githubs/P4-Audio", title="select a file",
         filetypes=(("PNG files", "*.png"), ("All files", "*"))
     )
+
+    filterImage = root.filename
+
     CV2Image = cv2.imread(root.filename, cv2.IMREAD_UNCHANGED)
     if CV2Image.dtype == np.uint16:
         CV2Image = (CV2Image / 256).astype('uint8')
@@ -36,9 +44,26 @@ def select():
     DisplayImage.pack()
 
     mb.showinfo("Sliders", "Use the sliders on the left to adjust the filter parameters")
+    print(filterImage)
+
 
 def Apply():
+    global filterImage
+    global CV2Image
+    global transformed_image
+    
     DisplayImage.destroy()
+
+    CV2Image = cv2.imread(root.filename, cv2.IMREAD_UNCHANGED)
+    if CV2Image.dtype == np.uint16:
+        CV2Image = (CV2Image / 256).astype('uint8')
+    CV2Image = cv2.cvtColor(CV2Image, cv2.COLOR_BGR2RGB)
+    CV2Image = PIL.Image.fromarray(CV2Image)
+    transformed_image = ImageTk.PhotoImage(CV2Image)
+    Test = Label(ImageFrame, image=transformed_image)
+    Test.pack()
+
+    SliderValues()
 
 def SliderValues():
     print(Slider1.get())
@@ -72,7 +97,7 @@ Slider2.grid(row=1, column=0)
 Slider3=Scale(SliderFrame, from_=0, to=50, tickinterval=15, orient=HORIZONTAL, troughcolor="darkred", background="red")
 Slider3.grid(row=2, column=0)
 
-Btn = Button(SliderFrame, text="Apply filter", command=lambda: [Apply(), SliderValues()])
+Btn = Button(SliderFrame, text="Apply filter", command=Apply)
 Btn.grid(row=0, column=1)
 
 ImageFrame = LabelFrame(OverFrame, text="This is the image", padx=5, pady=5)
